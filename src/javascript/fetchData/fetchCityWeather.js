ace88e3b040e8a5efd11718889d93b229de312fd
Blairon <230472458+Blairon1@users.noreply.github.com>
2026-09-21 09:39:08 -0400

    Feat: Webpage can fetch weather data from cities typed and entered in the search bar

--- src/javascript/fetchData/fetchCityWeather.js
+++ src/javascript/fetchData/fetchCityWeather.js
@@ -11,8 +11,31 @@ const weatherCityInput = document.querySelector("#city-input");
 // Implementation
 */
 
-// Fetch Weather data for the city selected
-async function fetchWeatherData() {}
+// Fetch Weather data for the city selected (Using Promises)
+function fetchWeatherData(city) {
+  const apiKey = "VSJJF7YPT92KEZD6LS3KXLS4V";
+  const location = city;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}`;
+
+  // Fetching data from endpoint
+  fetch(`${url}?unitGroup=metric&key=${apiKey}&contentType=json`)
+    .then((data) => {
+      // Promise chaining, checking if there's a problem with fetching, otherwise we will return json
+      if (!data.ok) {
+        throw new Error("Network response was not ok");
+      } else {
+        return data.json(); // raw HTTP response stream, reads it to completion, and parses the text body as JSON into a native JavaScript object
+      }
+    })
+    .then((data) => {
+      // Handle the data returned from the first then method call
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle any errors fetching the data
+      console.log(`Fetch Error:`, error);
+    });
+}
 
 // User presses enter after typing in a city
 weatherCityInput.addEventListener("keydown", (event) => {
@@ -20,5 +43,6 @@ weatherCityInput.addEventListener("keydown", (event) => {
     event.preventDefault();
 
     console.log("Enter key pressed! Current value:", event.target.value);
+    fetchWeatherData(event.target.value);
   }
 });
